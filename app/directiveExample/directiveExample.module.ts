e002729a526db05556a76ed9b0514240a7ce6e46
Matłosz <ajms@gft.com>
2017-02-24 16:29:03 +0100

    remove any directive reference

--- app/directiveExample/directiveExample.module.ts
+++ app/directiveExample/directiveExample.module.ts
@@ -3,14 +3,10 @@ import { CommonModule } from '@angular/common';
 import { FormsModule }   from '@angular/forms';
 import { DirectiveExampleComponent } from './directiveExample.component';
 import { ZoomTextDirective } from './zoomText.directive';
-import { AnyDirective } from './any.directive';
-
-import { PComponent } from './p.component';
-import { DivComponent } from './div.component';
 
 @NgModule({
     imports: [ CommonModule, FormsModule ],
-    declarations: [ DirectiveExampleComponent, ZoomTextDirective, AnyDirective, PComponent, DivComponent ],
+    declarations: [ DirectiveExampleComponent, ZoomTextDirective ],
     exports: [ DirectiveExampleComponent ]
 })
 export class DirectiveExampleModule { }
